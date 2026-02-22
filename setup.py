7539bc30d57c96e9798655f6943fbf689a55db17
Red Panda <redpandauser1995@gmail.com>
2026-02-21 17:15:59 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="torchcriterion",
-    version="0.1.0",
-    description="A modular PyTorch loss function library...",
-    author="TransformerTitan",
+    version="0.2.0",
+    description="A modular PyTorch loss function library.",
+    author="LiodonAI",
     packages=find_packages(),
     install_requires=["torch>=2.0,<3.0"],
     python_requires=">=3.7",
